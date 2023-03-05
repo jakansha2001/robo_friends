de72960f56ed2ac067f91191e6d95b38a868cccf
jakansha2001 <jakansha2001@gmail.com>
2023-03-06 01:22:42 +0530

    created basic UI

--- lib/home/pages/home_page.dart
+++ lib/home/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -16,14 +17,90 @@ class HomePage extends StatelessWidget {
                 begin: Alignment.centerLeft,
                 end: Alignment.centerRight,
                 colors: [
-                  Color.fromRGBO(73, 122, 125, 1),
+                  Color.fromRGBO(73, 123, 125, 1),
                   Color.fromRGBO(17, 28, 76, 1),
                 ],
               ),
             ),
           ),
-          const Scaffold(
+          Scaffold(
             backgroundColor: Colors.transparent,
+            body: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      top: 15.0,
+                    ),
+                    child: Text(
+                      'ROBOFRIENDS',
+                      style: TextStyle(
+                        fontFamily: 'Monoton',
+                        fontSize: 35,
+                        color: Color.fromRGBO(
+                          121,
+                          196,
+                          194,
+                          1,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 30,
+                      left: 50,
+                      right: 50,
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintStyle: GoogleFonts.roboto(),
+                        hintText: 'Search Robots',
+                        fillColor: Colors.white,
+                        filled: true,
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GridView.builder(
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                          ),
+                          itemCount: 10,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Card(
+                              color: const Color.fromRGBO(
+                                187,
+                                231,
+                                210,
+                                1,
+                              ),
+                              child: Center(child: Text('$index')),
+                            );
+                          }),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
